f76343b43e9acef12404b60b6cf826dfedd1f84a
Hongdp <hongdp@gmail.com>
2015-12-05 21:30:33 -0500

    update pa4

--- pa4_CPP/Index_server.h
+++ pa4_CPP/Index_server.h
@@ -5,14 +5,17 @@
 #include <stdint.h>
 #include <string>
 #include <vector>
+#include <set>
+#include <map>
 
 struct Query_hit {
 	Query_hit(const std::string& id_, double score_)
-        : id(id_), score(score_)
-        {}
-
+	: id(id_), score(score_), times(1)
+	{}
+	
 	std::string id;
-    double score;
+	double score;
+	int times;
 	bool operator< (const Query_hit& rhs) const{
 		return id < rhs.id;
 	}
@@ -20,11 +23,23 @@ struct Query_hit {
 
 class Index_server {
 public:
-    void run(int port);
-
-    // Methods that students must implement.
-    void init(std::ifstream& infile);
-    void process_query(const std::string& query, std::vector<Query_hit>& hits);
+	void run(int port);
+	
+	// Methods that students must implement.
+	void init(std::ifstream& infile);
+	void process_query(const std::string& query, std::vector<Query_hit>& hits);
+private:
+	struct weight{
+		int doc_id;
+		double weight;
+	};
+	
+	struct word_info{
+		double idf;
+		std::vector <weight> weights;
+	};
+	std::map<std::string,word_info> index_map;
+	std::set<std::string> stop_words;
 };
 
 #endif
